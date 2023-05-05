2bb5266cd4f99ac14c4ee65f18b7aff7d897aaaa
wekthor420 <wiktor6842@gmail.com>
2023-05-05 10:48:04 +0200

    dodanie github

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { google } = require('googleapis');
 const express = require('express')
 const OAuth2Data = require('./google_key.json')
+var queryString = require('querystring');
+const { default: axios } = require("axios");
 
 const app = express()
 
@@ -10,29 +12,39 @@ const REDIRECT_URL = 'https://autentykacja.onrender.com/auth/google/callback'
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
+var loggedBy=''
 app.get('/', (req, res) => {
     if(authed)
     {
-        var oauth2=google.oauth2({auth: oAuth2Client, version:'v2'})
-
-        oauth2.userinfo.v2.me.get(function(err,result)
+        if (loggedBy==='google')
         {
-            let loggedUser;
-            if (err) {
-                console.log("blad")
-                console.log(err)
-            } else {
-                loggedUser = result.data.name
-                console.log(loggedUser)
-            }
-            res.write('<a href="/googlelogout">wyloguj sie</a>')
-            res.end('Logged in: '.concat(loggedUser,'   <img src="',result.data.picture,'"height="23" width="23">'))
+            var oauth2=google.oauth2({auth: oAuth2Client, version:'v2'})
 
-        })
+            oauth2.userinfo.v2.me.get(function(err,result)
+            {
+                let loggedUser;
+                if (err) {
+                    console.log("blad")
+                    console.log(err)
+                } else {
+                    loggedUser = result.data.name
+                    console.log(loggedUser)
+                }
+                res.write('<a href="/googlelogout">wyloguj sie</a>')
+                res.end('Logged in: '.concat(loggedUser,'   <img src="',result.data.picture,'"height="23" width="23">'))
+
+            })
+        }else if (loggedBy==='github')
+        {
+            res.write('<a href="/googlelogout">wyloguj sie</a>')
+            res.end('logged by github')
+        }
+
 
     }else
     {
-        res.send('<a href="/google">zaloguj przez google</a>')
+        res.send('<a href="/google">zaloguj przez google</a><br/>' +
+            '<a href="/github">zaloguj przez github</a>')
 
     }
 })
@@ -41,6 +53,7 @@ app.get('/', (req, res) => {
 // });
 app.get('/googlelogout', (req, res) => {
     authed=false
+    loggedBy=''
     res.redirect('/');
 
 })
@@ -62,6 +75,19 @@ app.get('/google', (req, res) => {
     }
 })
 
+
+app.get('/github', (req, res) => {
+
+    console.log(REDIRECT_URL)
+    if (!authed) {
+        // Generate an OAuth URL and redirect there
+        res.redirect(`https://github.com/login/oauth/authorize?client_id=4f97a28ec1431c6b9fff`);
+    } else {
+        res.redirect('/');
+
+        // res.send('Logged in')
+    }
+})
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
     console.log("Adad")
@@ -78,10 +104,35 @@ app.get('/auth/google/callback', function (req, res) {
                 console.log('Successfully authenticated');
                 oAuth2Client.setCredentials(tokens);
                 authed = true;
+                loggedBy='google'
+
                 res.redirect('/')
             }
         });
     }
+});
+
+app.get('/auth/github/callback', function (req, res) {
+
+    axios.post("https://github.com/login/oauth/access_token", {
+        client_id: "4f97a28ec1431c6b9fff",
+        client_secret: "33b41e29a4542ce2df05282693290be2b578a40a",
+        code: req.query.code
+    }, {
+        headers: {
+            Accept: "application/json"
+        }
+    }).then((result) => {
+        // console.log(result.data.access_token)
+        // res.send("you are authorized " + result.data.access_token)
+        authed=true
+        loggedBy='github'
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    res.redirect('/')
+
 });
 
 const port = process.env.port || 3000
